aacc1c0437e4c09681ba83e98ba1c323586ffce0
Malix - Alix Brunet <alixbrunetcontact@gmail.com>
2026-03-01 16:54:47 +0100

    fix(flake): description

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Build env";
+  description = "Cross platform software to control the RGB/lighting of the 4 zone keyboard included in the 2020, 2021, 2022, 2023 and 2024 lineup of the Lenovo Legion laptops";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
